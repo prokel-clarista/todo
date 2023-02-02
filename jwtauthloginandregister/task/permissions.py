da4c1309af897ab1bc58101d329f946278b54cf9
Prokel Thaosen <119279822+prokel-clarista@users.noreply.github.com>
2023-02-02 20:27:37 +0530

    permission handling
    utilize default permission to be authenticated, user from request
    view permission will be allowed for user who created it or assigned to

--- jwtauthloginandregister/task/permissions.py
+++ jwtauthloginandregister/task/permissions.py
@@ -11,8 +11,12 @@ class AllButTaskCreatorGetReadOnly(permissions.BasePermission):
     edit_methods = ("PUT", "PATCH")
     def has_permission(self, request, view):
         try:
-            payload = jwt.decode(jwt=request.headers.authoriztation, key=SECRET_KEY, algorithms=['HS256'])
-            user = User.objects.get(token=payload["id"])
+#             payload = jwt.decode(jwt=request.headers.authoriztation, key=SECRET_KEY, algorithms=['HS256'])
+#             user = User.objects.get(token=payload["id"])
+            user = request.user # ensure you have applied permission classes on the view that allows only authenticated user
+            # moreover tokens will be valid for authenticated user only
+            # https://www.django-rest-framework.org/api-guide/permissions/#:~:text=REST_FRAMEWORK%20%3D%20%7B%0A%20%20%20%20%27DEFAULT_PERMISSION_CLASSES%27%3A%20%5B%0A%20%20%20%20%20%20%20%20%27rest_framework.permissions.IsAuthenticated%27%2C%0A%20%20%20%20%5D%0A%7D
+            
             if not user:
                 return False
         except Exception:
@@ -30,5 +34,7 @@ class AllButTaskCreatorGetReadOnly(permissions.BasePermission):
 
         if obj.created_by_id == payload["id"]:
             return True
+        # view/edit/delete permissions check for the user
+        # look for django guardian object permission
 
         return False
